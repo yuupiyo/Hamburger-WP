d4e3c0c3191f9afddda3d842206d6d57a081bd6b
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-10-11 01:22:03 -0700

    sidebar.php作成

---
+++ sidebar.php
@@ -0,0 +1,42 @@
+<!-- nav -->
+<aside class="l-sidebar p-sidebar c-grid__sidebar c-text-color__primary">
+      <!-- ハンバーガーボタンの×印 -->
+      <button class="p-sidebar__button c-close">
+        <span class="c-close__line"></span>
+        <span class="c-close__line"></span>
+      </button>
+
+      <h2 class="p-sidebar__title c-text__font01">Menu</h2>
+      <nav>
+        <dl class="c-nav__list">
+          <dt class="c-nav__title c-text__font02--bold"><a href="./archive.html">バーガー</a></dt>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">ハンバーガー</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">チーズバーガー</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">テリヤキバーガー</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">アボカドバーガー</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">フィッシュバーガー</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">ベーコンバーガー</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">チキンバーガー</a></dd>
+        </dl>
+
+        <dl class="c-nav__list">
+          <dt class="c-nav__title c-text__font02--bold"><a href="./archive.html">サイド</a></dt>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">ポテト</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">サラダ</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">ナゲット</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">コーン</a></dd>
+        </dl>
+
+        <dl class="c-nav__list">
+          <dt class="c-nav__title c-text__font02--bold"><a href="./archive.html">ドリンク</a></dt>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">コーラ</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">ファンタ</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">オレンジ</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">アップル</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">紅茶（Ice/Hot）</a></dd>
+          <dd class="c-nav__list-item c-text__font02"><a href="#">コーヒー（Ice/Hot）</a></dd>
+        </dl>
+      </nav>
+    </aside>
+    <!-- サイドバー後ろの黒い透過部分 -->
+    <div class="c-overlay"></div>
