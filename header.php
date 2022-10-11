5913b55e553495bfc5f7ed19fa826b6730b58c27
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-10-10 23:46:12 -0700

    header.phpの読み込み

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Raise Techの最終課題です">
+  <title>Hamburger</title>
+  <!-- css -->
+  <link rel="stylesheet" href="https://unpkg.com/destyle.css@3.0.2/destyle.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="c-grid">
+
+    <!-- header -->
+    <header class="c-grid__header l-header p-header">
+      <h1 class="p-header__title c-text__font01 c-text-color__primary">Hamburger</h1>
+
+      <form action="" class="p-search">
+        <label for="">
+          <input type="text" class="p-search__input c-border__round">
+        </label>
+        <label for="">
+          <input class="p-search__button c-text__font02--bold c-text-color__primary c-border__round c-button" type="submit" value="検索">
+        </label>
+      </form>
+
+      <!-- ハンバーガーボタン(Menu)部分 -->
+      <button class="p-header__button c-text__font01 c-text-color__primary">
+        Menu
+      </button>
+    </header>
