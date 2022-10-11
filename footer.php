e0d4900990807eaf1879625001fedf05e8d5a079
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-10-11 00:03:48 -0700

    footer.phpの読み込み

---
+++ footer.php
@@ -0,0 +1,15 @@
+    <!-- footer -->
+    <footer class="l-footer p-footer c-grid__footer c-text-color__secondary">
+      <ul class="p-footer__menu">
+        <li class="p-footer__menu-item c-text__font02">ショップ情報</li>
+        <li class="p-footer__menu-item c-text__font02">ヒストリー</li>
+      </ul>
+      <p class="p-footer__copyright c-text__font02"><small>Copyright: RaiseTech</small></p>
+    </footer> 
+  </div>
+
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -53,15 +53,6 @@
       </section>
     </article>
 
-    <!-- footer -->
-    <footer class="l-footer p-footer c-grid__footer c-text-color__secondary">
-      <ul class="p-footer__menu">
-        <li class="p-footer__menu-item c-text__font02">ショップ情報</li>
-        <li class="p-footer__menu-item c-text__font02">ヒストリー</li>
-      </ul>
-      <p class="p-footer__copyright c-text__font02"><small>Copyright: RaiseTech</small></p>
-    </footer>
-
     <!-- nav -->
     <aside class="l-sidebar p-sidebar c-grid__sidebar c-text-color__primary">
       <!-- ハンバーガーボタンの×印 -->
@@ -104,10 +95,5 @@
     </aside>
     <!-- サイドバー後ろの黒い透過部分 -->
     <div class="c-overlay"></div>
-  </div>
 
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-  <script src="js/script.js"></script>
-</body>
-</html>
+<?php get_footer(); ?>
