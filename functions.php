5105de4c62dbcfe2a0407d3abbdf13f910e1b17d
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-10-12 00:04:35 -0700

    タイトル出力

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
 <?php
+// テーマサポート
+add_theme_support('title-tag');
+
+// タイトル出力
+function hamburger_title($title) {
+  if(is_front_page() && is_home()) {
+    $title = get_bloginfo('name', 'display');
+  } elseif(is_singular()) {
+    $title = single_post_title('', false);
+  }
+  return $title;
+}
+add_filter('pre_get_document_title', 'hamburger_title');
+
 function add_files() {
   // リセットCSS
   wp_enqueue_style('reset-style', 'https://unpkg.com/destyle.css@3.0.2/destyle.min.css');
--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Raise Techの最終課題です">
-  <title>Hamburger</title>
   <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
   <div class="c-grid">
 
     <!-- header -->
