117431f27e02b35340d4168c79e1c8fe6dd6246f
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-10-21 00:36:44 -0700

    [update]value属性追加

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form method="get" action="<?php echo home_url('/'); ?>" class="p-search">
   <label for="">
-    <input type="text" class="p-search__input c-border__round" name="s" id="s" placeholder="キーワード">
+    <input type="text" class="p-search__input c-border__round" name="s" id="s" value="<?php the_search_query(); ?>" placeholder="キーワード">
   </label>
   <label for="">
-    <input class="p-search__button c-text__font02--bold c-text-color__primary c-border__round c-button" type="submit" value="検索">
+    <input class="p-search__button c-text__font02--bold c-text-color__primary c-border__round c-button" id="searchsubmit" type="submit" value="検索">
   </label>
 </form>
