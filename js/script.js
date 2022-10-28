5b11640f6f717cc72a3a46f3beb9595071ae7b0b
yuupiyo <yukari.jr.hiyoshi@gmail.com>
2022-10-28 01:43:00 -0700

    [clean]コード整理

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-jQuery(function() {
+jQuery(function () {
   // menuボタンを押した時の挙動
-  jQuery('.p-header__button').on('click', function() {
-    jQuery('.c-grid__sidebar').removeClass('is-close');
-    jQuery('.c-grid__sidebar').addClass('is-open');
-    jQuery('.c-overlay').toggleClass('is-open');
-    jQuery('body').toggleClass('fixed');
+  jQuery(".p-header__button").on("click", function () {
+    jQuery(".c-grid__sidebar").removeClass("is-close");
+    jQuery(".c-grid__sidebar").addClass("is-open");
+    jQuery(".c-overlay").toggleClass("is-open");
+    jQuery("body").toggleClass("fixed");
   });
 
   // ×ボタンを押した時の挙動
-  jQuery('.p-sidebar__button').on('click', function() {
-    jQuery('.c-grid__sidebar').removeClass('is-open');
-    jQuery('.c-overlay').removeClass('is-open');
-    jQuery('body').removeClass('fixed');
-    jQuery('.c-grid__sidebar').addClass('is-close');
-  });  
+  jQuery(".p-sidebar__button").on("click", function () {
+    jQuery(".c-grid__sidebar").removeClass("is-open");
+    jQuery(".c-overlay").removeClass("is-open");
+    jQuery("body").removeClass("fixed");
+    jQuery(".c-grid__sidebar").addClass("is-close");
+  });
 });
 
-jQuery(function() {
-  jQuery(window).on('resize', function() {
-    jQuery('.c-grid__sidebar').removeClass('is-open');
-    jQuery('.c-grid__sidebar').removeClass('is-close');
-    jQuery('.c-overlay').removeClass('is-open');
-    jQuery('body').removeClass('fixed');
+jQuery(function () {
+  jQuery(window).on("resize", function () {
+    jQuery(".c-grid__sidebar").removeClass("is-open");
+    jQuery(".c-grid__sidebar").removeClass("is-close");
+    jQuery(".c-overlay").removeClass("is-open");
+    jQuery("body").removeClass("fixed");
   });
 });
